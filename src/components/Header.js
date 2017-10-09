4a0f766b7542dd9def27b526044a86395695ad85
darin-estrin <darin.estrin@gmail.com>
2017-10-09 16:12:51 -0700

    fixed label for grade average and fixed spacing and line wrap issue for grade average on mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,11 @@ const Header = (props) => {
     return(
             <div className="page-header">
                 <h1 className='hidden-xs'>Student Grade Table
-                    <small> Grade Average : { props.average ? props.average : '0' } <span className="avgGrade label label-default"></span></small>
+                    <small> Grade Average : <span className="avgGrade label label-default">{ props.average ? props.average : '0' }</span></small>
                 </h1>
                 <h3 className="visible-xs">Student Grade Table
-                    <small> Grade Average : { props.average ? props.average : '0' } <span className="avgGrade label label-default"></span></small>
+                    <br />
+                    <small> Grade Average : <span className="avgGrade label label-default">{ props.average ? props.average : '0' }</span></small>
                 </h3>
             </div>
         )
