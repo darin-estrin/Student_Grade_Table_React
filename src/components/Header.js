400edbd074cb791593a0274f6e3ac26bd95da663
Darin Estrin <darin.estrin@gmail.com>
2018-04-13 12:26:28 -0700

    refactored header.js to use two spaces in stead of 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Header = (props) => {
 
-    return(
-            <div className="page-header">
-                <h1 className='hidden-xs'>Student Grade Table
-                    <small> Grade Average : <span className="avgGrade label label-default">{ props.average ? props.average : '0' }</span></small>
-                </h1>
-                <h3 className="visible-xs">Student Grade Table
-                    <br />
-                    <small> Grade Average : <span className="avgGrade label label-default">{ props.average ? props.average : '0' }</span></small>
-                </h3>
-            </div>
-        )
+	return (
+		<div className="page-header">
+			<h1 className='hidden-xs'>Student Grade Table
+				<small> Grade Average : <span className="avgGrade label label-default">{ props.average ? props.average : '0' }</span></small>
+			</h1>
+			<h3 className="visible-xs">Student Grade Table
+				<br />
+				<small> Grade Average : <span className="avgGrade label label-default">{ props.average ? props.average : '0' }</span></small>
+			</h3>
+		</div>
+	);
 }
 
 export default Header;
