a9a8f5f76b6aaab932c2d98111b3c2832a033217
Darin Estrin <darin.estrin@gmail.com>
2018-04-13 12:32:28 -0700

    refactored StudentDetails to be more readable

--- src/components/StudentDetails.js
+++ src/components/StudentDetails.js
@@ -1,27 +1,22 @@
 import React from 'react';
 
 const StudentDetails = (props) => {
-
-    const renderButton = function(){
-        if(!props.getDataClicked){
-            return <button className='btn btn-danger'
-            onClick={ (e) => props.handleDelete(e) } >Delete</button>
-        } else {
-            return <button className='btn btn-warning'
-            onClick={ (e) => props.handleUpdate(props) } >Update Grade</button>
-        }
-    }
-
-    return(
-        <tr id={ props.student.id }>
-           <td>{ props.student.name }</td>
-           <td>{ props.student.course }</td>
-           <td>{ props.student.grade }</td>
-           <td>
-            { renderButton() }
-           </td>
-        </tr>
-    )
+  
+  return (
+    <tr id={props.student.id}>
+      <td>{props.student.name}</td>
+      <td>{props.student.course}</td>
+      <td>{props.student.grade}</td>
+      <td>
+        <button
+          className='btn btn-danger btn-sm'
+          onClick={(e) => props.handleDelete(e)}>Delete</button>
+        <button
+          className='btn btn-warning btn-sm'
+          onClick={(e) => props.handleUpdate(props)}>Update</button>
+      </td>
+    </tr>
+  );
 }
 
 export default StudentDetails;
